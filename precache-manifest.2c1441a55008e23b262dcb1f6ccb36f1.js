e6e69e58ec0afdaf208819056a532a0019132a00
wahyu alfarisi <wahyualfarisi@gmail.com>
2019-04-25 17:49:45 +0700

    Updates

---
+++ precache-manifest.2c1441a55008e23b262dcb1f6ccb36f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57bbaa988837e25fc9afa310be5fc57a",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "31e5266cb0cfe285e930",
+    "url": "/movie/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/movie/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "31e5266cb0cfe285e930",
+    "url": "/movie/static/js/main.a3c0f6af.chunk.js"
+  },
+  {
+    "revision": "7954298017ed2d5ed8bc",
+    "url": "/movie/static/js/runtime~main.038a1c73.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie/static/media/logo.5d5d9eef.svg"
+  }
+]);
