a570fb7eec82531227a633f1fd2188a2711e9122
wahyu alfarisi <wahyualfarisi@gmail.com>
2019-05-02 13:52:59 +0700

    change store

--- src/store.js
+++ src/store.js
@@ -1,17 +1,17 @@
-import {applyMiddleware, createStore, compose} from 'redux';
-import thunk from 'redux-thunk';
-import rootReducers from './reducers';
+import { applyMiddleware, createStore, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducers from "./reducers";
 
 const middleware = [thunk];
 const initialState = {};
 
 const store = createStore(
-    rootReducers,
-    initialState,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+  rootReducers,
+  initialState,
+  compose(
+    applyMiddleware(...middleware)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 export default store;
